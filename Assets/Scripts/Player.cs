fa06c54ce9a40975f4fa567b0409991879e59b7a
fordiquez <rusak.ermakov@gmail.com>
2022-04-15 19:54:27 +0300

    game state & scoring

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -43,4 +43,12 @@ public class Player : MonoBehaviour
         }
         spriteRenderer.sprite = sprites[spriteIndex];
     }
+    
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.gameObject.tag == "Obstacle") {
+            FindObjectOfType<GameManager>().GameOver();
+        } else if (other.gameObject.tag == "Scoring") {
+            FindObjectOfType<GameManager>().IncreaseScore();
+        }
+    }
 }
